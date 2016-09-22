6e53410d996279fc1a23db24baddd2c8d5130578
nguyennhatthuan <nhatthuandiend@gmail.com>
2016-09-22 10:45:13 +0700

    thêm Register - 10:44 - 22/9/2016

--- WebCinema/WebCinema/Models/DataAccess/UserDAO.cs
+++ WebCinema/WebCinema/Models/DataAccess/UserDAO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using WebCinema.Models.Cinema;
@@ -13,6 +14,25 @@ namespace WebCinema.Models.DataAccess
         {
             UserAccount User = db.UserAccounts.SingleOrDefault(a => a.UserName == UserName && a.Password_ == Password);
             return User;
+        }
+
+        public int Register(string Username, string Password, string Email, string Phone)
+        {
+            try
+            {
+                UserAccount User = new UserAccount();
+                User.UserName = Username;
+                User.Password_ = Password;
+                User.Email = Email;
+                User.Phone = Phone;
+                db.UserAccounts.Add(User);
+                db.SaveChanges();
+                return 1;
+            }
+            catch(DataException)
+            {
+                return 0;
+            }
         }
     }
 }

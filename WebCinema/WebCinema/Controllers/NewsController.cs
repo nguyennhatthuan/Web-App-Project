d7f6dc61678f659b9b6bcca21fec72f2229cd553
jerem <jerem@KIRA-PC>
2016-09-23 03:30:50 +0700

    Bị lỗi ở News/Index - Chưa fix- để mai xem lại

--- WebCinema/WebCinema/Controllers/NewsController.cs
+++ WebCinema/WebCinema/Controllers/NewsController.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebCinema.Models.Cinema;
+using WebCinema.Models.DataAccess;
 
 namespace WebCinema.Controllers
 {
     public class NewsController : Controller
     {
+        MovieDbContext db = new MovieDbContext();
+        public ActionResult ShowNews(int count)
+        {
+            var news = from n in db.News select n;
+            return PartialView(news);
+        }
         // GET: News
         public ActionResult Index()
         {
